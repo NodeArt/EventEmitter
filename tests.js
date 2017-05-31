f9ad9cd1e4bd10b335862369efd6965d3752f71b
Ivan Prodaiko <prodaiko94@gmail.com>
2017-05-31 23:00:43 +0300

    Written tests;

--- tests.js
+++ tests.js
@@ -4,12 +4,26 @@
 "use strict";
 
 const assert = require('assert');
-const EventEmitter = require('./src/');
+const EventEmitter = require('./');
 
 const events = new EventEmitter();
 
-const ctx = { ctx : true };
+const [extend, inherit] = [{}, {}];
+// extend test
+assert.equal(
+  Object.getPrototypeOf(EventEmitter.extend(extend)),
+  Object.prototype,
+  'Prototype should stay the same (Array.prototype)'
+);
+// inherit test
+assert.equal(
+  Object.getPrototypeOf(EventEmitter.inherit(inherit)),
+  EventEmitter.prototype,
+  'Prototype should be EventEmitter prototype'
+);
 
+
+const ctx = { ctx : true };
 events
   .once(
     'event1',
@@ -24,7 +38,7 @@ events
   )
   .emit('event1', ctx, 1, 2, 3);
 
-assert.equal(Object.keys(events._events['event1']).length, 2, 'Once test error');
+assert.equal(events._events.get('event1').length, 2, 'Once test error');
 
 const fn = (...args) => assert.deepEqual(args, [1, 2, 3], 'Args test error');
 
@@ -33,10 +47,18 @@ events
   .emit('event2', null, 1, 2, 3)
   .off('event2', fn);
 
-assert.equal(events._events['event2'], undefined, 'Off test error');
+assert.equal(events._events.get('event2'), undefined, 'Off test error');
 
 events
   .on('event2', fn)
   .offAll();
 
-assert.deepEqual(events._events, {}, 'OffAll test error');
+assert.deepEqual(events._events, new Map(), 'OffAll test error');
+let counter = 0;
+events
+  .times('event', 3, () => counter += 1)
+  .emit('event')
+  .emit('event')
+  .emit('event');
+
+assert.equal(counter, 3, 'Times test error');
