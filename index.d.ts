221d2f8678737298807b484449578cf7b69c2f79
IvanProdaiko94 <prodaiko94@gmail.com>
2018-05-15 13:15:06 +0300

    reverted and changed version

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
 declare class EventEmitter {
   readonly _events: Map<string, {fn: Function, times?: number, count?: number}>
   once(eventName: string, ...fns: Function[]): this
-  times(eventName: string, times: number, ...fns: Function[]): this
+  times(eventName: string, times?: number, ...fns: Function[]): this
   on(eventName: string, ...fns: Function[]): this
-  emit(eventName: string, ctx: any = null, ...args: any[]): this
+  emit(eventName: string, ctx?: any = null, ...args: any[]): this
   off(eventName: string, ...fns: Function[]): this
   offAll(): this
   static inherit(successor: any): any
